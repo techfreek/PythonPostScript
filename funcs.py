9fc24c8a55c2a58e83d46288c80098cf4ee2251d
Alex Bahm <alexgbahm@gmail.com>
2013-09-11 21:10:13 -0700

    First commit, it may work

---
+++ funcs.py
@@ -0,0 +1,31 @@
+#Programmer: Alex Bahm
+global debug
+debug = False
+
+def add(a, b):
+
+return
+
+def sub(a, b):
+
+return
+
+def mul(a, b):
+
+return
+
+def div(a, b):
+
+return
+
+def eq(a, b):
+
+return
+
+def lt(a, b):
+
+return
+
+def gt(a, b):
+
+return
